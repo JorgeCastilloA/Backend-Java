07e7e2452bd9296e70ea76ea0ac6fbc576ed10f6
JorgeCastilloA <99519741+JorgeCastilloA@users.noreply.github.com>
2022-04-25 18:41:35 -0500

    update: PrimerActo.java

--- practicaComic/PrimerActo.java
+++ practicaComic/PrimerActo.java
@@ -2,9 +2,9 @@ package practicaComic;
 
 public interface PrimerActo {
     void titulo();
-    void billyInicio();//Primer panel
-    void clarkInicio();//Segundo panel
-    void llegadaBilly();//Billy llega al restaurante
+    void billyInicio();
+    void clarkInicio();
+    void llegadaBilly();
     void ataqueRestaruante();
     void ataqueRestauranteDos();
     void ataqueRestauranteTres();
