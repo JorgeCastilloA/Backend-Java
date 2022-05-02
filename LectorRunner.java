61b0d4d1554a978205db5ab4f84d00647e5ce3fc
JorgeCastilloA <99519741+JorgeCastilloA@users.noreply.github.com>
2022-05-01 20:22:00 -0500

    Update LectorRunner.java

--- LectorRunner.java
+++ LectorRunner.java
@@ -10,16 +10,9 @@ public class LectorRunner {
         Screen screen = new Screen();
         Dialog d = new Dialog();
         String str;
-        //screen.out(); to print out
-        //screen.setVisible(true); to make it visible
-
-        // str = d.readString("Ejemplo");
-
-
+   
         //Inicia Primera Parte del Comic
-        //  screen.out("¿Quieres iniciar el comic?\n( Y / N )");
-        // screen.setVisible(true);
-
+        
         str = d.readString("¿Quieres iniciar el comic?\n( Y / N )");
         if (str.charAt(0) == 'Y' || str.charAt(0) == 'y') {
             Comic1 comic1 = new Comic1();
@@ -125,97 +118,5 @@ public class LectorRunner {
             screen.setVisible(true);
 
 
-/*
-        String respuesta2 = null;
-        screen.out("¿Quieres continuar con el siguiente capítulo?\n (Y/N)", Colors.FussionRed);
-        screen.setVisible(true);
-        Scanner scanner = new Scanner(System.in);
-        respuesta2 = scanner.next();
-
-        if(respuesta2.toUpperCase() == "Y"){
-            screen.out("Cúal es el capítulo con el que quieres continuar?\nElige 2 0 3");
-            screen.setVisible(true);
-            Scanner scanner2 = new Scanner(System.in);
-            int numCap = scanner2.nextInt();
-            if (numCap == 2){
-                //instancia obj que corra cap 2
-            }else if (numCap==3){
-                //instancia que corra cap 3
-            }else
-                scanner2.close();
-        }else if (respuesta2.toUpperCase() == "N"){
-            screen.out("Fin de Lectura");
-            screen.setVisible(true);
-
-            //mensaje de terminar comic
-        }else
-            screen.out("Respuesta invalida\n Elige entre (Y/N)");
-            screen.setVisible(true);
-             scanner.close();
-    }
-
-
-    */
-
-        /*
-        String respuesta = scanner.next();
-
-        scanner.close();
-
-        transicion(respuesta);
-    }
-
-    private static void transicion(String respuesta){
-      if(respuesta == "Y"){
-          System.out.println("Cúal es el capítulo con el que quieres continuar?\nElige 2 0 3");
-          Scanner scanner2 = new Scanner(System.in);
-          int numCap = scanner2.nextInt();
-          if (numCap == 2){
-              //instancia obj que corra cap 2
-          }else if (numCap==3){
-              //instancia que corra cap 3
-          }else
-              scanner2.close();
-      }else if (respuesta == "N"){
-          //mensaje de terminar comic
-      }else
-          System.out.println("Respuesta inválida\n Elige entre (Y/N)");
-    }*/
-
-
-        }/*
-    private static void continuarComic(String iniciar, String continuar, String terminar){
-
-        if(iniciar.toUpperCase() == "Y"){
-            Comic1 comic1 = new Comic1();
-            comic1.titulo();
-            comic1.billyInicio();
-            comic1.clarkInicio();
-            comic1.llegadaBilly();
-            comic1.ataqueRestaruante();
-            comic1.blackAdam();
-            comic1.supermanInicia();
-        }else return;
-        if(continuar.toUpperCase()=="Y"){
-            Comic2 comic2 = new Comic2();
-            comic2.supermanAtaca();
-            comic2.billyEsconde();
-            comic2.cuevaShazam();
-            comic2.historiaShazam();
-            comic2.shazamInicia();
-            comic2.blackAdamAtaca();
-            comic2.shazamDefiende();
-        }else return;
-        if (terminar.toUpperCase()=="Y"){
-            Comic3 comic3 = new Comic3();
-            comic3.shazamAnaliza();
-            comic3.adamAtaca();
-            comic3.supermanDefiende();
-            comic3.heroesUnidos();
-            comic3.heroesAtacan();
-            comic3.heroesGanan();
-            comic3.terminaComic();
-}
-    }*/
-
+        }
     }
